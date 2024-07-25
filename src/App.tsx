a38abafb2b14ca11ac723e6aebaef802e3fb5bbb
Morning Dove Nelson <morningdhansen@gmail.com>
2024-07-25 09:26:15 -0600

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Calculator from "./Calculator";
+
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Tip Calculator</h1>
+      </header>
+      <main>
+        <Calculator />
+      </main>
+      <footer>
+        <p>Created By: Morning Dove Nelson 2024</p>
+      </footer>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Calculator.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+
+const Calculator: React.FC = () => {
+    let [billAmount, setBillAmount] = useState("");
+    let [tipPercentage, setTipPercentage] = useState("");
+
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Calculator;
